8d3f2d3528fe90148cbd7600d1e7a17a6e32ee37
rohanbachhav <rohanbachhav10@gmail.com>
2024-12-28 02:18:31 +0530

    Escaped the Apostrophes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ const About = () => {
     <div className="container">
       <h1>who am i</h1>
       <p>
-        I'm Rohan Bachhav, a Master's student in IT with a strong background in programming and software engineering. I have experience in various programming languages like C++, Python, and JavaScript, and I'm particularly interested in competitive programming and algorithm design.
+        I&apos;'m Rohan Bachhav, a Master&apos;'s student in IT with a strong background in programming and software engineering. I have experience in various programming languages like C++, Python, and JavaScript, and I&apos;'m particularly interested in competitive programming and algorithm design.
       </p>
       <p>
         My focus areas include:
@@ -15,7 +15,7 @@ const About = () => {
         <li>Research in machine learning, NLP, and blockchain technologies.</li>
       </ul>
       <p>
-        Currently, I’m diving deeper into C#, and I plan to explore creating my own programming language for fun. I also have a keen interest in building high-performance systems, like an order execution and management system for Deribit Test.
+        Currently, I&apos;’m diving deeper into C#, and I plan to explore creating my own programming language for fun. I also have a keen interest in building high-performance systems, like an order execution and management system for Deribit Test.
       </p>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div className="container">
-      <h1>rohan's site </h1>
+      <h1>rohan&apos;'s site </h1>
       <p>
-        Hello, I'm Rohan Bachhav, a passionate IT professional with a focus on programming and software engineering. I specialize in competitive programming, game development, and various IT domains, including machine learning and blockchain. 
+        Hello, I&apos;'m Rohan Bachhav, a passionate IT professional with a focus on programming and software engineering. I specialize in competitive programming, game development, and various IT domains, including machine learning and blockchain. 
         <Link href="/about">Learn more about me</Link>.
       </p>
       <Link href="/projects">projects : [ there are none ]</Link>
